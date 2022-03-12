789485fe9efbe94528067dfd76caebebe4e99c66
donatto-minaya <ottanod22@gmail.com>
2022-03-12 02:49:15 -0500

    Detalles actualizados

--- src/other/OS.js
+++ src/other/OS.js
@@ -1,11 +1,16 @@
 export const OS = {
     /**
+     * Web function
     Return the operating system
     @return {string} Windows, Linux, Android ...
     **/
     getOS: function() {
         return navigator.userAgentData.platform
     },
+
+    /**
+    * Web function    
+    **/
 
     isMobile: function() {
         return navigator.userAgentData.mobile
