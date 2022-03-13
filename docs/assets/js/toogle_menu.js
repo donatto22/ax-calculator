d3c1165896eb392a5b0d06f4c6c4c3c0c742369d
donatto-minaya <ottanod22@gmail.com>
2022-03-13 15:56:18 -0500

    Get started page finished

--- docs/assets/js/toogle_menu.js
+++ docs/assets/js/toogle_menu.js
@@ -10,9 +10,10 @@ toggles menu visibility, and blur
 function toggleMenu() {
     menuToggle.classList.toggle('toggle-active')
 
-    blur2 == 0 ? blur2 = 10 : blur2 = 0
+    blur2 == 0 ? blur2 = 8 : blur2 = 0
 
     document.documentElement.style.setProperty('--blur', `${blur2}px`)
+    document.getElementById("particles").classList.toggle('particles-effect')
 }
 
 buttonToggleMenu.addEventListener("click", toggleMenu)
