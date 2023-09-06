86642f3777987f7e80c4238c6004d909e9614b28
donatto22 <ottanod22@gmail.com>
2023-09-06 16:01:36 -0500

    Se mejora el validador de regex
    Ahora la función de texto permite un rango de letras usando length y se creó una función de length implicito donde un texto debe tener si o si cierta cantidad de carácteres.

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,1 +1,10 @@
 import { RegexValidator } from '../dist/main.js' 
+
+const test = RegexValidator.onlyLetters({
+    length: {
+        minvalue: 0,
+        maxvalue: 3
+    }
+}).test('axxc')
+
+console.log(test)
--- src/other/RegexValidator.ts
+++ src/other/RegexValidator.ts
@@ -1,12 +1,21 @@
 export const RegexValidator = {
     /**
      * @param capitalize If true, you allos capital letters
+     * @param length You can put the min value and max value for the length
      * @returns The regex pattern that allows only letters
      */
     onlyLetters: function (object: {
-        capitalize: boolean
+        capitalize: boolean,
+        length: {
+            minvalue: Number,
+            maxvalue: Number
+        }
     }) {
-        return object?.capitalize ? /^[a-zA-Z]+$/ : /^[a-z]+$/
+        if(object.length !== undefined) {
+            return object.capitalize ? 
+            new RegExp(`^[a-zA-Z]{${object.length.minvalue},${object.length.maxvalue}}$`) : 
+            new RegExp(`^[a-z]{${object.length.minvalue},${object.length.maxvalue}}$`)
+        } else return object.capitalize ? /^[a-zA-Z]+$/ : /^[a-z]+$/
     },
 
     /**
@@ -26,15 +35,59 @@ export const RegexValidator = {
         return object?.capitalize ? /[a-zA-Z0-9]+$/ : /[a-z0-9]+$/
     },
 
-    // /**
-    //  * 
-    //  * @param minvalue 
-    //  * @returns The regex pattern
-    //  */
-    // length: function (object: {
-    //     minvalue: Number, maxvalue: Number
-    // }) {
-    //     // `/^.{${object?.minvalue}, ${object?.maxvalue}}`
-    //     return /^.{1, 20}$/
-    // }
+    /**
+     * @param minvalue the min length
+     * @param maxvalue the max length
+     * @returns The regex pattern
+     */
+    length: function (object: {
+        minvalue: Number, maxvalue: Number 
+    }) {
+        return new RegExp(`^.{${object.minvalue},${object.maxvalue}}$`)
+    },
+
+    /**
+     * 
+     * @param lengths You can put more than one length
+     * @returns The regex pattern
+     */
+
+    implicitLength: function (...lengths: Array<Number>) {
+        // si tiene solo un elemento
+        if (lengths.length === 1) {
+            return new RegExp(`^.{${lengths[0]}}$`)
+        } 
+        
+        // en caso haya más
+        else {
+            let conditionals = ''
+
+            // se obtiene el ultimo elemento
+            let ultimoElemento = `.{${lengths[lengths.length - 1]}}`
+
+            // y se elimina el último del array
+            lengths.pop()
+
+            // se agregan cada uno de los elementos
+            lengths.forEach(element => {
+                conditionals += `.{${element}}|`
+            });
+
+            // agregamos manualmente el ultimo
+            conditionals += ultimoElemento
+            
+            // console.log(conditionals);
+            return new RegExp(`^(${conditionals})$`)
+        }
+    },
+
+    /**
+     * 
+     * @param text The string you want coincidence
+     * @returns The regex pattern
+     * @example RegexValidator.coicidence('abc').test('Hello abc') // true
+     */
+    coindicence: function (text: string) {
+        return new RegExp(`/${text}/`)
+    }
 }
